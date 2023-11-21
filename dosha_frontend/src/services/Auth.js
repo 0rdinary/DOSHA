587276d53f5ecd10f2d08671d6eedd2f0c9d2ea2
0rdinary <ac980925@gmail.com>
2023-11-21 20:32:03 +0900

    proxy test

--- dosha_frontend/src/services/Auth.js
+++ dosha_frontend/src/services/Auth.js
@@ -80,13 +80,10 @@ export const checkToken = (key) => {
 
 export const requestLogin = async ({ id, password }) => {
   try {
-    const response = await axios.post(
-      process.env.REACT_APP_DB_HOST + '/auth/login',
-      {
-        id,
-        password,
-      },
-    );
+    const response = await axios.post('/auth/login', {
+      id,
+      password,
+    });
     console.log(response);
     const { accessToken, refreshToken, name, position } = response.data;
     return {
