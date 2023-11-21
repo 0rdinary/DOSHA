b07ddd6d9cf9481912fc559b61941bb32bd13e9f
0rdinary <ac980925@gmail.com>
2023-11-22 01:13:38 +0900

    adsf

--- dosha_backend/src/main/java/com/dgb/dosha/domain/educations/EducationsService.java
+++ dosha_backend/src/main/java/com/dgb/dosha/domain/educations/EducationsService.java
@@ -20,7 +20,7 @@ public class EducationsService extends EgovAbstractServiceImpl {
 	
 	private final EducationsRepository er;
 	private final EmployeeRepository employeeRepository;
-	String filePath = "/educations/";
+	String filePath = "\\home\\ubuntu\\DOSHA\\educations\\";
 	
 	public void upload(Long id, MultipartFile educations) {
 		String originFileName = educations.getOriginalFilename();
