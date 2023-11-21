c1fce9d17bfabc6aad36708c15e7e0654818923c
0rdinary <ac980925@gmail.com>
2023-11-21 21:29:37 +0900

    cors change

--- dosha_backend/src/main/java/com/dgb/dosha/global/config/WebConfig.java
+++ dosha_backend/src/main/java/com/dgb/dosha/global/config/WebConfig.java
@@ -2,13 +2,18 @@ package com.dgb.dosha.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 	 @Override
 	    public void addCorsMappings(CorsRegistry registry) {
 		 registry.addMapping("/**")
-         .allowedOrigins("*");
+         .allowedOrigins("*")
+         .allowedHeaders("*")
+         .allowedMethods("*")
+         .allowCredentials(false);
 	    }
 }
